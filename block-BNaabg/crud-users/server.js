e768f886245127e54091112ce6eb25705b7defc7
Vinoo <ivinoop@gmail.com>
2022-03-07 15:35:16 +0530

    refactor

--- block-BNaabg/crud-users/server.js
+++ block-BNaabg/crud-users/server.js
@@ -1,20 +1,22 @@
 const http = require('http');
 const fs = require('fs');
-const qs = require('querystring');
+const path = require('path');
+const url = require('url');
 
-const userDir = path.join(__dirname, '/users');
+const userDir = __dirname + '/users/';
 
 let server = http.createServer(handleRequest);
 
 function handleRequest(req, res) {
   let parsedUrl = url.parse(req.url, true);
-  let user = parsedUrl.query.username;
+  let username = parsedUrl.query.username;
   let store = '';
   req
   .on('data', (chunk) => {
     store += chunk;
   })
   .on('end', () => {
+    // CREATE
     if(req.method === 'POST' && req.url === '/users') {
       let username = JSON.parse(store).username;
       fs.open(userDir + username + '.json', 'wx', (err, fd) => {
@@ -22,23 +24,26 @@ function handleRequest(req, res) {
         fs.writeFile(fd, store, (err) => {
           if(err) console.log(err);
           fs.close(fd, () => {
-            res.end(`${username} successfully created`);
+            return res.end(`${username} successfully created`);
           });
         });
       });
     }
+    // READ
     else if(req.method === 'GET' && parsedUrl.pathname === '/users') {
       res.setHeader('content-type', 'application/json');
-      return fs.createReadStream(userDir + user + '.json').pipe(res);
+      return fs.createReadStream(userDir + username + '.json').pipe(res);
     }
+    // DELETE
     else if(req.method === 'DELETE' && parsedUrl.pathname ==='/users') {
-      fs.unlink(userDir + user + '.json', (err) => {
+      fs.unlink(userDir + username + '.json', (err) => {
         if(err) console.log(err);
-        return res.end(`${user} is deleted`);
+        return res.end(`${username} is deleted`);
       });
     }
+    // UPDATE
     else if(req.method === 'PUT' && parsedUrl.pathname === '/users') {
-      fs.open(userDir + user + '.json', 'r+', (err, fd) => {
+      fs.open(userDir + username + '.json', 'r+', (err, fd) => {
         if(err) console.log(err);
         fs.ftruncate(fd, (err) => {
           if(err) console.log(err);
@@ -46,12 +51,13 @@ function handleRequest(req, res) {
             if(err) console.log(err);
             fs.close(fd, (err) => {
               if(err) console.log(err);
-              return res.end(`${user} updated successfully`);
+              return res.end(`User "${username}" updated successfully`);
             })
           })
         })
       })
     }
+    // Handle Error
     else {
       res.statusCode = 404;
       res.end('Page not found');
